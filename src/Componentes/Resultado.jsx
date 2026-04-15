65104934f96a526146d5d748aa307e2c861c37d4
Rafael <araujobonaldirafael@gmail.com>
2026-04-15 11:56:05 -0300

    Com Css

--- src/Componentes/Resultado.jsx
+++ src/Componentes/Resultado.jsx
@@ -1,11 +1,15 @@
-export default function Resultado({ total, desconto}){
-    if(total === null) return null;
-
-  return(
-    desconto === "" ? (
-      <p>Não foi selecionado desconto. Valor total: R$ {total.toFixed(2)}</p>
-    ) : (
-      <p>Valor com desconto aplicado: R$ {total.toFixed(2)}</p>
-    )
+export default function Resultado({ total, desconto }){
+  if(total === null) return null;
+  
+  const valorComDesconto = total * (1 - desconto/100);
+  
+  return (
+    <div className="result-container">
+      {desconto === "" ? (
+        <p>Não foi selecionado desconto. Valor total: R$ {total.toFixed(2)}</p>
+      ) : (
+        <p>Valor com desconto: R$ {valorComDesconto.toFixed(2)}</p>
+      )}
+    </div>
   )
 }
--- src/Componentes/SeletorCliente.jsx
+++ src/Componentes/SeletorCliente.jsx
@@ -1,11 +1,13 @@
 export default function SeletorCliente({desconto, setDesconto}){
-    return(
-        <select onChange={(e) => setDesconto(e.target.value)}>
-      <option value="">Escolha ...</option>
-      <option value="30">Funcionário (30%)</option>
-      <option value="20">VIP (20%)</option>
-      <option value="15">Parceiro (15%)</option>
-      <option value="5">Comum (5%)</option>
-    </select>
-    )
+  return(
+    <div className="select-container">
+      <select className="select" onChange={(e) => setDesconto(e.target.value)}>
+        <option value="">Escolha ...</option>
+        <option value="30">Funcionário (30%)</option>
+        <option value="20">VIP (20%)</option>
+        <option value="15">Parceiro (15%)</option>
+        <option value="5">Comum (5%)</option>
+      </select>
+    </div>
+  )
 }
